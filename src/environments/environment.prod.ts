ed8c306e83da91f5083d1d40dbf519546a9638fe
gudimallaavinash0999 <gudimallaavinash0999@gmail.com>
2019-10-21 18:35:01 +0530

    update1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAWfAS_hdeJXS0Casa-RZFJ6iS_gdlx0XA",
+    authDomain: "online1-68df2.firebaseapp.com",
+    databaseURL: "https://online1-68df2.firebaseio.com",
+    projectId: "online1-68df2",
+    storageBucket: "online1-68df2.appspot.com",
+    messagingSenderId: "364551028872",
+    appId: "1:364551028872:web:1e4931836fd7359757df3d",
+    measurementId: "G-JV3PW4PTFE"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebase : {
+    apiKey: "AIzaSyAWfAS_hdeJXS0Casa-RZFJ6iS_gdlx0XA",
+    authDomain: "online1-68df2.firebaseapp.com",
+    databaseURL: "https://online1-68df2.firebaseio.com",
+    projectId: "online1-68df2",
+    storageBucket: "online1-68df2.appspot.com",
+    messagingSenderId: "364551028872",
+    appId: "1:364551028872:web:1e4931836fd7359757df3d",
+    measurementId: "G-JV3PW4PTFE"
+
+
+  }
 };
 
 /*
